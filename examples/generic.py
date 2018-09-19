6aad7f2437a7f52939f28547b27e5f0e1bf395d0
Tim Olson <timjolson@users.noreply.github.com>
2018-09-18 19:25:42 -0500

    testing

--- examples/generic.py
+++ examples/generic.py
@@ -1,4 +1,5 @@
 from PyQt5.QtCore import QPoint, QSize
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QWidget, QLineEdit, QLabel, QPushButton, QComboBox
 from PyQt5.Qt import QApplication
 
@@ -8,38 +9,76 @@ from adjustableWidget import DraggableWidget, AdjustableWidget, DragButtons
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-app = QApplication([])
 
-win = QWidget()
-win.setGeometry(200,200,400,500)
+class DeleteableWidget():
+    def __init__(self, *args, **kwargs):
+        print('deletable init')
+        print(kwargs)
+        super().__init__(*args, **kwargs)
 
-class Button(QPushButton, DraggableWidget):
+    def keyPressEvent(self, QKeyEvent):
+        if QKeyEvent.key() == Qt.Key_Delete:
+            self.deleteLater()
+        else:
+            super(type(self), self).keyPressEvent(QKeyEvent)
+
+class Button(QPushButton, DeleteableWidget, DraggableWidget):
+# class Button(QPushButton, DraggableWidget, DeleteableWidget):
+    # keyPressEvent = deleteEvent
     pass
 
 class EditBox(QLineEdit, AdjustableWidget):
     pass
 
-class Label(QLabel, AdjustableWidget):
+class Label(QLabel, DeleteableWidget, AdjustableWidget):
+    # keyPressEvent = deleteEvent
     pass
 
 class Combo(QComboBox, AdjustableWidget):
     def __init__(self, parent=None, **kwargs):
+        print('combo init')
+        print(kwargs)
         AdjustableWidget.__init__(self, parent, **kwargs)
         self.addItems([*'1234567890'])
 
-e = EditBox(win, button=DragButtons.MID)
-e.move(50,100)
 
-B = Button(win)
+app = QApplication([])
+
+# win = QWidget()
+win = AdjustableWidget(button=DragButtons.MID, objectName='win')
+win.setGeometry(200,200,400,500)
+
+print('adjustable done')
+
+
+E = EditBox(win, button=DragButtons.MID, objectName='E')
+E.move(50,100)
+E.setMinimumSize(QSize(50,20))
+# e.setMaximumSize(QSize(150,200))
+# e.setFixedSize(QSize(50,20))
+# e.setFixedHeight(20)
+# e.setFixedWidth(80)
+
+print('editbox done')
+
+B = Button(win, objectName='B')
 B.setText('Button Object')
 B.resize(100, 80)
 
-L = Label(B)
+print('button done')
+
+L = Label(B, objectName='L')
 L.setText('Label Object')
 L.setFrameStyle(1)
 
-C = Combo(e, button=DragButtons.RIGHT)
+print('label done')
+
+C = Combo(E, button=DragButtons.RIGHT, objectName='C')
 C.move(80,0)
+
+print('combo done')
+
+# TODO: debug combo box size jump
 
 win.show()
 app.exec_()
